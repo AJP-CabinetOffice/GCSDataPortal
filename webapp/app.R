f098739145d9ac4b704a1f4ce56fca227db62a7f
Ashley Poole <ashley.poole@cabinetoffice.gov.uk>
2022-05-16 17:42:08 +0100

    Large code update with added features
    - Fixed bugs about checking columns
    - Changed column configurations to match the latest Data Audit design
    - Added nil return handling
    - Added email submission
    - Added more helpful submission message

--- webapp/app.R
+++ webapp/app.R
@@ -3,15 +3,17 @@ library(magrittr)
 # Load functions.
 source("functions.R")
 
+debug = T
+
 ## Set up AWS credentials and variables
 
-# env_vcap_services <- Sys.getenv("VCAP_SERVICES") %>% rjson::fromJSON()
-# 
-# Sys.setenv("AWS_ACCESS_KEY_ID" = env_vcap_services$`aws-s3-bucket`[[1]]$credentials$aws_access_key_id,
-#            "AWS_SECRET_ACCESS_KEY" = env_vcap_services$`aws-s3-bucket`[[1]]$credentials$aws_secret_access_key,
-#            "AWS_DEFAULT_REGION" = env_vcap_services$`aws-s3-bucket`[[1]]$credentials$aws_region)
+env_vcap_services <- Sys.getenv("VCAP_SERVICES") %>% rjson::fromJSON()
 
-# s3_bucket = env_vcap_services$`aws-s3-bucket`[[1]]$credentials$bucket_name
+Sys.setenv("AWS_ACCESS_KEY_ID" = env_vcap_services$`aws-s3-bucket`[[1]]$credentials$aws_access_key_id,
+           "AWS_SECRET_ACCESS_KEY" = env_vcap_services$`aws-s3-bucket`[[1]]$credentials$aws_secret_access_key,
+           "AWS_DEFAULT_REGION" = env_vcap_services$`aws-s3-bucket`[[1]]$credentials$aws_region)
+
+s3_bucket = env_vcap_services$`aws-s3-bucket`[[1]]$credentials$bucket_name
 
 object_log = "rgcs_people_continuous/logs/submission_log.csv"
 
@@ -19,29 +21,64 @@ object_log = "rgcs_people_continuous/logs/submission_log.csv"
 col_check_prefix <- "check_passed_"
 
 # Read in the configuration file.
-col_config <- 
+col_config_raw <-
   readr::read_csv(
-    "2022-05-05 Column configuration OFFICIAL - Sheet 1.csv",
+    "2022-05-11 Column configuration OFFICIAL - Sheet 1 (5).csv",
     col_types = readr::cols(.default = "c")
-  ) %>% 
+  ) %>%
+  ## Remove withdrawn columns
   dplyr::filter(status == "Included")
 
+## Generate regex for the discrete columns.
+discrete_regexes <-
+  makeDiscreteChoicesAllowedValues(col_config_raw) %>%
+  makeRegexFromAllowedValues()
+
+org_regex <-
+  readxl::read_excel(
+    "2022-05-10_16-01-00_BST Organisations list for DoCs.xlsx",
+    sheet = "Organisations Table",
+    col_types = "text",
+    .name_repair = janitor::make_clean_names
+  ) %>%
+  dplyr::pull(organisation_code_gcs_data_audit) %>%
+  c("99", "**") %>%
+  makeRegexFromCharacterVector()
+
+col_config <-
+  col_config_raw %>%
+  tibble::add_column(discrete_regexes) %>%
+  dplyr::mutate(discrete_regexes = as.character(discrete_regexes)) %>%
+  dplyr::rowwise() %>%
+  dplyr::mutate(
+    allowed_regex = dplyr::case_when(
+      completion_mode == "discrete_choices_large" ~ org_regex,
+      discrete_regexes != "NULL" ~ discrete_regexes,!is.na(allowed_regex_initial) ~ allowed_regex_initial,
+      T ~ NA_character_
+    )
+  ) %>%
+  dplyr::ungroup()
+
 # For use by quality checks
-allowed_colnames <- 
+allowed_colnames <-
   col_config$col_id
 
 # Define UI
 ui <- shiny::fluidPage(
   shiny::column(1),
-  shiny::column(10,
+  shiny::column(
+    10,
     shiny::br(),
     shiny::br(),
     shiny::titlePanel("GCS Data Portal"),
     shiny::br(),
-    shiny::HTML("This data portal is for use in the GCS Data Audit."),
+    shiny::HTML("This data portal is for use in the GCS Data Audit 2022."),
     shiny::br(),
-    shiny::fileInput("file1",
-                     "Upload your file:"),
+    shiny::br(),
+    shiny::textInput("email1", "Enter an email address (shared mailbox preferred)"),
+    shiny::uiOutput("panel_email"),
+    shiny::br(),
+    shiny::uiOutput("file_upload"),
     shiny::br(),
     shiny::uiOutput("panel_feedback"),
     shiny::br(),
@@ -52,32 +89,34 @@ ui <- shiny::fluidPage(
 
 # Define server logic
 server <- function(input, output) {
+  checkEmailAddressIsValidReactive <- shiny::reactive({
+    message(input$email1)
+    checkEmailAddressIsValid(input$email1, debug)
+  })
   
-  success <- shiny::reactiveVal(T)
-  
-  checkColumnContents <- function(
-    col_name,
-    col_type,
-    allowed_length_per_entry = NULL
-  ){
-    
-    
-    full_result <- list(
-      result = result,
-      message = message
+  output$file_upload <- shiny::renderUI({
+    shiny::validate(
+      shiny::need(
+        checkEmailAddressIsValidReactive(),
+        "Enter a valid email address to continue."
+      )
     )
     
-    return(full_result)
-  }
+    fileInput("file1",
+              "Upload your file:")
+  })
+  
   
   readData <- shiny::reactive({
-    
     shiny::req(input$file1$datapath)
     
-    if(input$file1$type == "text/csv"){
+    if (input$file1$type == "text/csv") {
       data <- tryCatch(
         readr::read_csv(input$file1$datapath),
-        error = function(e){return(F)}) 
+        error = function(e) {
+          return(F)
+        }
+      )
     } else {
       data <- F
     }
@@ -86,16 +125,28 @@ server <- function(input, output) {
   })
   
   didDataReadReactive <- shiny::reactive({
-    if(tibble::is_tibble(readData())){
+    if (tibble::is_tibble(readData())) {
       T
     } else {
       F
     }
   })
   
-  checkAllColumnsContentsReactive <- shiny::reactive({  
-    checkAllColumnsContents(readData(), col_config)
-    })
+  detectNilReturnReactive <- shiny::reactive({
+    detectNilReturn(readData(), debug)
+  })
+  
+  checkAllColumnsArePresentReactive <- shiny::reactive({
+    checkAllColumnsArePresent(readData(), col_config, debug)
+  })
+  
+  checkNoExtraColsReactive <- shiny::reactive({
+    checkNoExtraCols(readData(), col_config)
+  })
+  
+  checkAllColumnsContentsReactive <- shiny::reactive({
+    checkAllColumnsContents(readData(), col_config, debug)
+  })
   
   didAllColumnsContentsPassReactive <- shiny::reactive({
     df <- checkAllColumnsContentsReactive()
@@ -103,53 +154,126 @@ server <- function(input, output) {
   })
   
   wasSubmissionAcceptedReactive <- shiny::reactive({
-    results_cols <- 
-      c(didAllColumnsContentsPassReactive())
+    if (detectNilReturnReactive()) {
+      overall_result <- T
+    } else {
+      results <-
+        c(
+          checkAllColumnsArePresentReactive(),
+          checkNoExtraColsReactive(),
+          didAllColumnsContentsPassReactive(),
+        )
+      
+      overall_result <- as.logical(prod(results))
+      
+    }
     
-    overall_result <- as.logical(prod(results_cols))
     return(overall_result)
+    
   })
   
   output$panel_feedback <- shiny::renderUI({
     result_data_readin <- didDataReadReactive()
     
-    result_cols <- if(result_data_readin){
-      wasSubmissionAcceptedReactive()
+    if (debug) {
+      message("Did data read in as a tibble? ", result_data_readin)
+    }
+    
+    result_nil_return <- if (result_data_readin) {
+      detectNilReturnReactive()
     } else {
       F
     }
     
+    if (debug) {
+      message("Was it a nil return? ", result_nil_return)
+    }
     
-    # writeSubmissionToS3()
-    if(result_cols){
-      shiny::HTML("Success! Thank you for your submission.")
-    } else if(!result_data_readin){
-      shiny::HTML("Your submission was not accepted.<br>All submissions must be in CSV format. Please resubmit in the correct format.")
-    } else if(!result_cols){
-      shiny::HTML("Your submission was not accepted.<br>Items that require your attention are higlighted in yellow.<br>Please correct the highlighted data and resubmit.")
-      
+    result_all_cols_present <-
+      if (result_data_readin & !result_nil_return) {
+        checkAllColumnsArePresentReactive()
+      } else {
+        F
+      }
+    
+    if (debug) {
+      message("Were all expected columns present? ",
+              result_all_cols_present)
+    }
+    
+    result_no_extra_cols <-
+      if (result_data_readin & !result_nil_return) {
+        checkNoExtraColsReactive()
+      } else {
+        F
+      }
+    
+    if (debug) {
+      message("Were there no extra columns? ", result_no_extra_cols)
+    }
+    
+    result_column_contents <-
+      if (result_all_cols_present & result_no_extra_cols) {
+        didAllColumnsContentsPassReactive()
+      } else {
+        F
+      }
+    
+    if (debug) {
+      message("Was the content of the cells acceptable? ",
+              result_column_contents)
+    }
+    
+    writeSubmissionToS3()
+    
+    if (result_data_readin &
+        result_all_cols_present &
+        result_no_extra_cols & result_column_contents) {
+      shiny::HTML("Success!<br>Thank you for your submission.")
+    } else if (result_data_readin & result_nil_return) {
+      shiny::HTML("Success! Nil return received.<br>Thank you for your submission.")
+    } else if (!result_data_readin) {
+      shiny::HTML(
+        "Your submission was not accepted.<br>All submissions must be in CSV format. Please resubmit in the correct format."
+      )
+    } else if (!result_all_cols_present) {
+      shiny::HTML(
+        "Your submission was not accepted.<br>Some data columns are missing.<br>Please add the missing data columns and resubmit."
+      )
+    } else if (!result_no_extra_cols) {
+      shiny::HTML(
+        "Your submission was not accepted.<br>Extra data columns were detected.<br>Please remove the extra columns and resubmit."
+      )
+    } else if (!result_column_contents) {
+      shiny::HTML(
+        "Your submission was not accepted.<br>Items that require your attention are higlighted in yellow.<br>Please correct the highlighted data and resubmit."
+      )
     }
   })
   
   output$datatable <- DT::renderDataTable({
     shiny::req(didDataReadReactive())
-    result_cols <- wasSubmissionAcceptedReactive()
-    shiny::req(!result_cols)
+    shiny::req(checkAllColumnsArePresentReactive())
+    shiny::req(checkNoExtraColsReactive())
+    shiny::req(!detectNilReturnReactive())
+    
+    result <- wasSubmissionAcceptedReactive()
+    
+    shiny::req(!result)
+    
     df_tested <- checkAllColumnsContentsReactive()
-
-    col_names_raw_data <- 
+    
+    col_names_raw_data <-
       colnames(df_tested)[colnames(df_tested) %in% col_config$col_id]
     
     col_names_qa <-
       colnames(df_tested)[colnames(df_tested) %>% stringr::str_detect(pattern = paste0("^", col_check_prefix))]
     
-    qa_col_indices <- 
-      match(
-        col_names_qa,
-        colnames(df_tested)
-      )
+    qa_col_indices <-
+      match(col_names_qa,
+            colnames(df_tested))
     
-    df_tested %>% 
+    df_tested %>%
       DT::datatable(options = list(
         paging = F,
         searching = F,
@@ -157,31 +281,31 @@ server <- function(input, output) {
         columnDefs = list(list(
           visible = F, targets = qa_col_indices
         ))
-      )) %>% 
-      DT::formatStyle(
-        col_names_raw_data,
-        col_names_qa,
-        backgroundColor = DT::styleEqual(c(T, F), c("white", "yellow")))
+      )) %>%
+      DT::formatStyle(col_names_raw_data,
+                      col_names_qa,
+                      backgroundColor = DT::styleEqual(c(T, F), c("white", "yellow")))
   })
   
   passesColNameCheck <- shiny::reactive({
-    
-    result_cols <- 
-      readData() %>% 
-      colnames() %>% 
-      purrr::map(.f = function(colname){
-        res = F
-        if(colname %in% allowed_colnames){
-          res = T
+    result <-
+      readData() %>%
+      colnames() %>%
+      purrr::map(
+        .f = function(colname) {
+          res = F
+          if (colname %in% allowed_colnames) {
+            res = T
+          }
+          return(res)
         }
-        return(res)
-      })
+      )
     
-    overall_result = prod(as.numeric(result_cols))
+    contents_result = prod(as.numeric(result))
     
-    return(overall_result)
+    return(contents_result)
   })
-
+  
   
   UpdateSubmissionLog <- shiny::reactive({
     entity_code = "TEST_TEST"
@@ -214,45 +338,56 @@ server <- function(input, output) {
   
   writeSubmissionToS3 <- shiny::reactive({
     shiny::req(input$file1$datapath)
-    result_cols <- wasSubmissionAcceptedReactive()
+    result <- wasSubmissionAcceptedReactive()
     
-    sysdatetime <- Sys.time() %>% 
+    sysdatetime <- Sys.time() %>%
       format("%Y-%m-%dT%H:%M:%S_%Z")
     
-    folder_qa <- if(result_cols){
+    folder_qa <- if (result) {
       "qa_passed"
     } else {
       "qa_failed"
     }
     
-    file_uniqueness <- input$file1$datapath %>% 
-      stringr::str_split(pattern = "/") %>% 
-      unlist() %>% 
-      tail(n = 2) %>% 
+    file_uniqueness <- input$file1$datapath %>%
+      stringr::str_split(pattern = "/") %>%
+      unlist() %>%
+      tail(n = 2) %>%
       head(n = 1)
     
     folder_name <- paste(sysdatetime, file_uniqueness)
     
-    full_filename_raw <- 
+    full_filename_raw <-
       paste(folder_qa, folder_name, "RAW OFFICIAL-SENSITIVE.csv", sep = "/")
-  
-    aws.s3::put_object(
-      input$file1$datapath,
-      full_filename_raw,
-      s3_bucket
-    ) 
     
-    analysis_result <- checkAllColumnsContentsReactive()
+    aws.s3::put_object(input$file1$datapath,
+                       full_filename_raw,
+                       s3_bucket)
     
-    full_filename_qa <- 
-      paste(folder_qa, folder_name, "QA OFFICIAL-SENSITIVE.csv", sep = "/")
+    if(!detectNilReturnReactive()){
+      analysis_result <- checkAllColumnsContentsReactive()
+      
+      full_filename_qa <-
+        paste(folder_qa, folder_name, "QA OFFICIAL-SENSITIVE.csv", sep = "/")
+      
+      aws.s3::s3write_using(analysis_result,
+                            readr::write_excel_csv,
+                            object = full_filename_qa,
+                            bucket = s3_bucket)
+    }
     
-    aws.s3::s3write_using(
-      analysis_result,
-      readr::write_excel_csv,
-      object = full_filename_qa,
-      bucket = s3_bucket
-    )
+    submitter_email <- 
+      input$email1 %>% 
+      as.data.frame()
+    
+    full_filename_email <-
+      paste(folder_qa, folder_name, "CONTACT EMAIL OFFICIAL-SENSITIVE.csv", sep = "/")
+    
+    aws.s3::s3write_using(submitter_email,
+                          readr::write_excel_csv,
+                          object = full_filename_email,
+                          bucket = s3_bucket)
+
   })
 }
 
--- webapp/functions.R
+++ webapp/functions.R
@@ -1,39 +1,123 @@
-checkAllColumnsContents <- function(
-  df,
-  col_config
-){
+checkEmailAddressIsValid <- function(email_to_check, debug) {
   
-  df_intermediate <- 
+  result <-
+    stringr::str_detect(email_to_check, pattern = "^.{1,}@.{1,}$")
+  
+  if (debug){
+    func_name <- sys.call()[1]
+    message(func_name, "\tEmail address submitted = ", email_to_check)
+    message(func_name, "\tDid the email address pass the test? ", result)
+  }
+  
+  return(result)
+}
+
+checkAllColumnsArePresent <- function(df, col_config, debug) {
+  columns_in_data <- colnames(df)
+  
+  columns_expected <- col_config$col_id
+  
+  names(columns_expected) <- columns_expected
+  
+  columns_present <-
+    columns_expected  %>%
+    purrr::map_df(.f <- function(.x) {
+      .x %in% columns_in_data
+    }) %>%
+    tidyr::pivot_longer(dplyr::everything())
+  
+  result <-
+    columns_present %>%
+    dplyr::summarise(overall_result = prod(value)) %>%
+    dplyr::pull(overall_result) %>%
+    as.logical()
+  
+  if (debug) {
+    func_name <- sys.call()[1]
+    message(func_name, "\tName of check = ", func_name)
+    message(func_name, "\tResult of check = ", result)
+    message(func_name,
+            "\tCol names in submission = ",
+            paste(columns_in_data, collapse = ", "))
+    message(func_name,
+            "\tExpected col names = ",
+            paste(columns_expected, collapse = ", "))
+    str <-  c()
+    for (i in 1:nrow(columns_present)) {
+      str_new <-
+        paste(columns_present[1][[1]][i], columns_present[2][[1]][i], collapse = " = ")
+      str <- append(str, str_new)
+    }
+    message(func_name,
+            "\tPresence of each column =\n\t\t",
+            paste(str, collapse = "\n\t\t"))
+  }
+  
+  return(result)
+  
+}
+
+checkNoExtraCols <- function(df, col_config) {
+  columns_in_data <- colnames(df)
+  
+  columns_expected <- col_config$col_id
+  
+  names(columns_in_data) <- columns_in_data
+  
+  columns_present <-
+    columns_in_data  %>%
+    purrr::map_df(.f <- function(.x) {
+      .x %in% columns_expected
+    }) %>%
+    tidyr::pivot_longer(dplyr::everything()) %>%
+    dplyr::summarise(overall_result = prod(value)) %>%
+    dplyr::pull(overall_result) %>%
+    as.logical()
+}
+
+checkAllColumnsContents <- function(df,
+                                    col_config,
+                                    debug) {
+  df_intermediate <-
     df
   
-  col_names <- 
-    col_config$col_id
+  col_order <-
+    tibble::tibble(data_col_names = colnames(df_intermediate)) %>%
+    dplyr::mutate(col_order_in_data = dplyr::row_number())
   
-  col_mode <- 
-    col_config$completion_mode
+  col_config_reordered <-
+    col_config %>%
+    dplyr::left_join(col_order, by = c("col_id" = "data_col_names")) %>%
+    dplyr::arrange(col_order_in_data)
   
-  regex_checks <- 
-    col_config$allowed_regex
+  col_names <-
+    col_config_reordered$col_id
+  
+  col_mode <-
+    col_config_reordered$completion_mode
+  
+  regex_checks <-
+    col_config_reordered$allowed_regex
   
   ## These next two are only used for numeric columns
-  range_min <- 
-    col_config$numeric_range_min
+  range_min <-
+    col_config_reordered$numeric_range_min
   
-  range_max <- 
-    col_config$numeric_range_max
+  range_max <-
+    col_config_reordered$numeric_range_max
   
-  for(i in 1:length(regex_checks)){
+  for (i in 1:length(regex_checks)) {
     # Numeric ranges are handled differently.
     # Numeric ranges are marked as "continuous_number" in the config file; handle them first by checking if it is in the correct range.
     # If it isn't a "continuous_number" then use the provided regex to check instead.
-    if(col_mode[i] == "continuous_number"){
-      
+    if (col_mode[i] == "continuous_number") {
       df_intermediate <-
         df_intermediate %>%
         dplyr::mutate(
-          ## Try to see if the number is between two values. 
+          ## Try to see if the number is between two values.
           ## If a warning occurs it is likely because the raw value cannot be coerced to a number, so it fails the test.
           "check_passed_{col_names[i]}_numeric" := dplyr::case_when(
+            stringr::str_detect(!!rlang::sym(col_names[i]), pattern = "^\\*\\*$") ~ T,
             is.na(as.numeric(!!rlang::sym(col_names[i]))) ~ F,
             T ~ dplyr::between(!!rlang::sym(col_names[i]), range_min[i], range_max[i])
           )
@@ -42,50 +126,142 @@ checkAllColumnsContents <- function(
       df_intermediate <-
         df_intermediate %>%
         dplyr::mutate(
-          "check_passed_{col_names[i]}_regex" := stringr::str_detect(!!rlang::sym(col_names[i]), pattern = unlist(regex_checks[i]))
+          "check_passed_{col_names[i]}_regex" := dplyr::case_when(
+            is.na(!!rlang::sym(col_names[i])) ~ F,
+            T ~ stringr::str_detect(!!rlang::sym(col_names[i]), pattern = unlist(regex_checks[i]))
+          )
         )
     }
   }
-  
   df_result = df_intermediate
+  
+  if (debug) {
+    
+  }
   
   return(df_result)
 }
 
-didAllColumnsContentsPass <- function(
-  df,
-  col_config,
-  col_check_prefix
-){
-  col_names <- 
+didAllColumnsContentsPass <- function(df,
+                                      col_config,
+                                      col_check_prefix) {
+  col_names <-
     col_config$col_id
   
-  passed_checks <- 
-    df %>% 
-    dplyr::select(
-      dplyr::starts_with(col_check_prefix)
-    ) %>% 
-    dplyr::summarise(
-      dplyr::across(dplyr::everything(), ~ as.logical(prod(.x)))
-    ) %>%
-    tidyr::pivot_longer(dplyr::everything()) %>%
+  passed_checks <-
+    df %>%
+    dplyr::select(dplyr::starts_with(col_check_prefix)) %>%
+    dplyr::summarise(dplyr::across(dplyr::everything(), ~ as.logical(prod(.x)))) %>%
+    tidyr::pivot_longer(dplyr::everything())
+  
+  passed_checks <-
+    passed_checks %>%
     ## If there is an NA value, then assume that the test failed. The na value
     ## is because a test has not been specified for that column.
-    dplyr::mutate(value = dplyr::case_when(
-      is.na(value) ~ F,
-      T ~ T
-    )) %>% 
+    dplyr::mutate(value = dplyr::case_when(is.na(value) ~ F,
+                                           T ~ value))
+  
+  passed_checks <-
+    passed_checks %>%
     dplyr::summarise(check_passed_ALL = as.logical(prod(value))) %>%
     dplyr::pull(check_passed_ALL)
-
+  
   return(passed_checks)
 }
 
-generateUserMessageColumnContents <- function(columns_contents_passed){
-  if(columns_contents_passed){
-    message = NULL
-  } else {
-    message = "Invalid values were detected in your submission. The table below shows the data that you submitted. Entries coloured yellow were detected as invalid."
+generateUserMessageColumnContents <-
+  function(columns_contents_passed) {
+    if (columns_contents_passed) {
+      message = NULL
+    } else {
+      message = "Invalid values were detected in your submission. The table below shows the data that you submitted. Entries coloured yellow were detected as invalid."
+    }
+    return(message)
   }
-  return(message)
+
+makeDiscreteChoicesAllowedValues <- function(col_config) {
+  valid_item_codes <-
+    col_config$options %>%
+    purrr::map2(
+      .y = col_config$completion_mode,
+      .f = function(.x, .y) {
+        if (.y == "discrete_choices") {
+          item_labels = unlist(stringr::str_split(string = .x, pattern = ";"))
+          
+          extra_list_items = c("Other", "Unknown")
+          
+          `Item Label` <- c(item_labels, extra_list_items)
+          
+          ## Generate item codes to match the labels
+          item_codes <- 10:(10 + length(item_labels) - 1)
+          
+          extra_item_codes <- c(99, "**")
+          
+          final_item_codes <- c(item_codes, extra_item_codes)
+        }
+        
+      }
+    )
+  
+  names(valid_item_codes) <- col_config$col_id
+  return(valid_item_codes)
+}
+
+makeRegexFromCharacterVector <- function(char_vec) {
+  if (is.character(char_vec)) {
+    starting_body <- stringr::str_c(char_vec, collapse = "$|^")
+    
+    regex_with_start_and_end <- paste0("^", starting_body, "$")
+    
+    regex_replace_stars <-
+      regex_with_start_and_end %>%
+      stringr::str_replace_all(pattern = stringr::fixed("*"),
+                               replacement = "\\*")
+    
+    regex_final <- regex_replace_stars
+  } else {
+    regex_final <- NULL
+  }
+  
+  return(regex_final)
+}
+
+makeRegexFromAllowedValues <- function(allowed_codes) {
+  allowed_regexes <-
+    allowed_codes %>%
+    purrr::map(makeRegexFromCharacterVector)
+}
+
+detectNilReturn <- function(df, debug) {
+  ## Start by checking that the ORG column exists.
+  result <-
+    if (!is.null(df$ORG)) {
+      ## Don't accept files with more than one row.
+      if (nrow(df) == 1) {
+        df_is_na <-
+          df %>%
+          dplyr::mutate(dplyr::across(dplyr::everything(), ~ is.na(.))) %>%
+          dplyr::select(-ORG) %>%
+          tidyr::pivot_longer(dplyr::everything())
+        
+        result <-
+          df_is_na %>%
+          dplyr::summarise(overall_result = prod(value)) %>%
+          dplyr::pull(overall_result) %>%
+          as.logical()
+      } else {
+        F
+      }
+      
+    } else {
+      F
+    }
+  
+  if (debug) {
+    func_name <- sys.call()[1]
+    message(func_name, "\t\tName of check = ", func_name)
+    message(func_name, "\t\tNumber of rows in dataframe = ", nrow(df))
+  }
+  
+  return(result)
 }
